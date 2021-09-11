154b47ffcc7035121ee0615b962d9910b7a5ae4b
pbl.gllgs <pbl.gllgs@gmail.com>
2021-09-11 15:55:14 -0300

    add resources

---
+++ src/main/resources/import.sql
@@ -0,0 +1,5 @@
+/* Populate tables */
+insert into employee (name, email,job_title, phone, image_url) values ('Pablo','pbl.gllgs@gmail.com','JAVA','957592973','https://www.bootdey.com/img/Content/avatar/avatar1.png');
+insert into employee (name, email,job_title, phone, image_url) values ('Carlos','Carlos@gmail.com','ANGULAR','54875896','https://www.bootdey.com/img/Content/avatar/avatar2.png');
+insert into employee (name, email,job_title, phone, image_url) values ('Mario','Mario@gmail.com','DATABASE','124598658','https://www.bootdey.com/img/Content/avatar/avatar3.png');
+insert into employee (name, email,job_title, phone, image_url) values ('Luis','Luis@gmail.com','TYPESCRIPT','758962485','https://www.bootdey.com/img/Content/avatar/avatar4.png');
