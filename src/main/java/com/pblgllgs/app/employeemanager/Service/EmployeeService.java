13ce51a61f7200e4d6e52712b06d1251535a414f
pbl.gllgs <pbl.gllgs@gmail.com>
2021-09-12 00:18:09 -0300

    Corregimos y agregamos @transactional

--- src/main/java/com/pblgllgs/app/employeemanager/Service/EmployeeService.java
+++ src/main/java/com/pblgllgs/app/employeemanager/Service/EmployeeService.java
@@ -5,6 +5,7 @@ import com.pblgllgs.app.employeemanager.model.Employee;
 import com.pblgllgs.app.employeemanager.repo.EmployeeRepo;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 import java.util.UUID;
@@ -19,24 +20,28 @@ public class EmployeeService {
         this.employeeRepo = employeeRepo;
     }
 
+    @Transactional
     public Employee addEmployee(Employee employee){
         employee.setEmployeeCode(UUID.randomUUID().toString());
         return employeeRepo.save(employee);
     }
 
+    @Transactional(readOnly = true)
     public List <Employee> findAllEmployees(){
         return employeeRepo.findAll();
     }
 
+    @Transactional
     public Employee updateEmployee(Employee employee){
         return employeeRepo.save(employee);
     }
 
+    @Transactional(readOnly = true)
     public Employee findEmployeeById(Long id){
        return employeeRepo.findEmployeeById(id).orElseThrow(
                () -> new UserNotFoundException("USUARIO by id " + id + "no fue encontrado"));
     }
-
+    @Transactional
     public void deleteEmployee(Long id){
         employeeRepo.deleteEmployeeById(id);
     }
