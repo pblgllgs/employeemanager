c69c636e08ae5e582fd65b729aa953a4c81954e0
pbl.gllgs <pbl.gllgs@gmail.com>
2021-09-17 16:29:29 -0300

    optimize imports

--- src/main/java/com/pblgllgs/app/employeemanager/controller/EmployeeResource.java
+++ src/main/java/com/pblgllgs/app/employeemanager/controller/EmployeeResource.java
@@ -7,7 +7,6 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.ArrayList;
 import java.util.List;
 
 @RestController
@@ -23,13 +22,13 @@ public class EmployeeResource {
 
     @GetMapping("/all")
     public ResponseEntity<List<Employee>> getAllEmployees(){
-        List <Employee> employees = employeeService.findAllEmployees();;
+        List <Employee> employees = employeeService.findAllEmployees();
         return new ResponseEntity<>(employees, HttpStatus.OK);
     }
 
     @GetMapping("/find/{id}")
     public ResponseEntity<Employee> getEmployee(@PathVariable("id") Long id){
-        Employee employee = employeeService.findEmployeeById(id);;
+        Employee employee = employeeService.findEmployeeById(id);
         return new ResponseEntity<>(employee, HttpStatus.OK);
     }
 
